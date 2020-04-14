15d98ef63f40215a0bd54d116064b3b455a26077
ramon.ribeiro90@gmail.com <ramon.ribeiro90@gmail.com>
2020-04-14 11:35:59 -0300

    antes entity core

--- QuickBuy.Dominio/Entidades/Pedido.cs
+++ QuickBuy.Dominio/Entidades/Pedido.cs
@@ -1,10 +1,11 @@
 ﻿using QuickBuy.Dominio.ObjetoDeValor;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace QuickBuy.Dominio.Entidades
 {
-    public class Pedido
+    public class Pedido : Entidade
     {
         public int Id { get; set; }
         public DateTime DataPedido { get; set; }
@@ -25,5 +26,21 @@ namespace QuickBuy.Dominio.Entidades
         /// </summary>
         public ICollection<ItemPedido> ItensPedido { get; set; }
 
+        public override void Validate()
+        {
+            LimparMensagensValidacao();
+            if (!ItensPedido.Any())
+            {
+                AdicionarCritica("Crítica - Pedido não pode ficar sem item de pedido");
+            }
+            if (string.IsNullOrEmpty(CEP))
+            {
+                AdicionarCritica("Crítica - CEP deve estar preenchido");
+            }
+            if (FormaPagamentoId == 0)
+            {
+                AdicionarCritica("Crítica - Não foi informado a forma de pagamento");
+            }
+        }
     }
 }
--- QuickBuy.Dominio/Entidades/Usuario.cs
+++ QuickBuy.Dominio/Entidades/Usuario.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 
 namespace QuickBuy.Dominio.Entidades
 {
-    public class Usuario
+    public class Usuario : Entidade
     {
         public int Id { get; set; }
         public string Email { get; set; }
@@ -15,6 +15,16 @@ namespace QuickBuy.Dominio.Entidades
         /// </summary>
         public ICollection<Pedido> Pedidos { get; set; }
 
-
+        public override void Validate()
+        {
+            if (string.IsNullOrEmpty(Email))
+            {
+                AdicionarCritica("Email não foi informado");
+            }
+            if (string.IsNullOrEmpty(Senha))
+            {
+                AdicionarCritica("Senha não foi informada");
+            }
+        }
     }
 }
