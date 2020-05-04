741e80e788ca308e12c49d823f33874fb54dde4f
ramon.ribeiro90@gmail.com <ramon.ribeiro90@gmail.com>
2020-05-04 13:07:32 -0300

    commit note last

--- QuickBuy.Web/ClientApp/src/app/usuario/login/login.component.ts
+++ QuickBuy.Web/ClientApp/src/app/usuario/login/login.component.ts
@@ -1,21 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Usuario } from '../../modelo/usuario';
-
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent{
+export class LoginComponent implements OnInit {
 
   public usuario;
+  public returnUrl: string;
 
-  constructor() {
+  constructor(private router: Router, private activatedRouter: ActivatedRoute) { //injeção de dependencia
+    //this.returnUrl = this.activatedRouter.snapshot.queryParams['returnUrl'];
+  }
+
+  ngOnInit(): void {
     this.usuario = new Usuario();
+    this.returnUrl = this.activatedRouter.snapshot.queryParams['returnUrl'];
   }
 
   entrar() {
+    if (this.usuario.email == 'teste@teste.com' && this.usuario.senha == '123') {
+      sessionStorage.setItem("usuario-autenticado", "1");
+      this.router.navigate([this.returnUrl]);
+    }
     alert(this.usuario.email + ' - ' + this.usuario.senha);
   }
 }
